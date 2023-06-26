679c4018be2ed2cb07c5d981cde6da16217f6260
seyfo32 <75393169+seyfo32@users.noreply.github.com>
2023-06-26 16:24:51 +0300

    News-Homepage
    News Homepage Responsive designed. (HTML CSS JavaScript)

---
+++ FEM-News Homepage/index.js
@@ -0,0 +1,11 @@
+var closedMenu = document.getElementById("closed-mobil-menu");
+var opacityB=document.getElementById("opacity-background");
+
+function openMenu(){
+    closedMenu.style.display="block";
+    opacityB.style.display="block";
+}
+function closeMenu(){
+    closedMenu.style.display="none";
+    opacityB.style.display="none";
+}
